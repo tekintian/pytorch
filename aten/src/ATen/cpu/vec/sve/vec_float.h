5cbafc504b1574c8946fe34eec6a5c71854e82dd
Nikita Shulga <nshulga@meta.com>
2026-03-03 09:21:27 -0800

    Update (base update)
    [ghstack-poisoned]

--- aten/src/ATen/cpu/vec/sve/vec_float.h
+++ aten/src/ATen/cpu/vec/sve/vec_float.h
@@ -22,7 +22,7 @@ namespace at::vec {
 // accessed as `at::vec`.
 inline namespace CPU_CAPABILITY {
 
-#if defined(CPU_CAPABILITY_SVE256)
+#if defined(CPU_CAPABILITY_SVE)
 
 template <>
 struct is_vec_specialized_for<float> : std::bool_constant<true> {};
@@ -764,7 +764,7 @@ Vectorized<float> inline fnmsub(
   return svnmad_f32_x(ptrue, a, b, c);
 }
 
-#endif // defined(CPU_CAPABILITY_SVE256)
+#endif // defined(CPU_CAPABILITY_SVE)
 
 } // namespace CPU_CAPABILITY
 } // namespace at::vec
