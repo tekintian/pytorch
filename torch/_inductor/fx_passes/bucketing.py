a55b1ffe43424da5ed08c3072edc6f0148f48471
IvanKobzarev <ivan.kobzarev@gmail.com>
2026-03-06 03:53:41 -0800

    [inductor][comms] memory budget for pg allocs
    ghstack-source-id: ed939079176931536a5ccedbdda695da3d92b2ac
    Pull-Request: https://github.com/pytorch/pytorch/pull/176172

--- torch/_inductor/fx_passes/bucketing.py
+++ torch/_inductor/fx_passes/bucketing.py
@@ -721,8 +721,6 @@ def _pre_bucket_all_gather(
     ]
     ag_input_numel = sum(ins_split_sizes)
     device = ag_ins[0].device
-    total_size_bytes = sum(ins_split_sizes_bytes) * group_size
-
     if torch._inductor.config.comms_use_pg_alloc:
         pg = _resolve_process_group(group_name)  # type: ignore[arg-type]
         backend = pg._get_backend(device)
@@ -1129,6 +1127,7 @@ def merge_reduce_scatter_bucket(
     rs0 = rs_nodes[0]
     rs0_val = rs0.meta["val"]
     _, reduce_op, group_size, group_name = rs0.args
+    assert isinstance(group_name, str)
     reduce_dtype = rs0_val.dtype
     device = rs0_val.device
 
@@ -1182,6 +1181,7 @@ def merge_all_reduce_bucket(
     ar0 = ar_nodes[0]
     ar0_val = ar0.meta["val"]
     _, reduce_op, group_name = ar0.args
+    assert isinstance(group_name, str)
     reduce_dtype = ar0_val.dtype
     device = ar0_val.device
 
